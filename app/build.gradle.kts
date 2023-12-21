e3642bc3fe494ef346e16971a807d5dced889368
laobocheng <bocheng.lao@viu.com>
2023-12-21 11:38:28 +0800

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,27 @@
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.com.google.gms.google.services)
+    alias(libs.plugins.com.google.firebase.crashlytics)
 }
 
 android {
+    signingConfigs {
+        val jksFile = System.getenv("XB_JKS_FILE")
+        val jksPassword = System.getenv("XB_JKS_PASSWORD")
+        getByName("debug") {
+            storeFile = file(jksFile)
+            storePassword = jksPassword
+            keyPassword = jksPassword
+            keyAlias = "debug"
+        }
+        create("release") {
+            storeFile = file(jksFile)
+            storePassword = jksPassword
+            keyPassword = jksPassword
+            keyAlias = "rhythm"
+        }
+    }
     namespace = "io.github.xiaobaicz.rhythm"
     compileSdk = 34
 
@@ -18,7 +36,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
@@ -39,6 +61,10 @@ dependencies {
     implementation(libs.bundles.common)
     testImplementation(libs.bundles.test)
     androidTestImplementation(libs.bundles.test.android)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
 
     implementation(libs.mmkv)
 
